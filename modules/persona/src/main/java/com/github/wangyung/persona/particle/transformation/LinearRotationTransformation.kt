d2b833240780e709c8f9d3d3be05800980162a07
Freddie <wangyung@gmail.com>
2021-09-02 11:33:31 +0900

    Add LinearRotationTransformationTest

--- modules/persona/src/main/java/com/github/wangyung/persona/particle/transformation/LinearRotationTransformation.kt
+++ modules/persona/src/main/java/com/github/wangyung/persona/particle/transformation/LinearRotationTransformation.kt
@@ -3,7 +3,9 @@ package com.github.wangyung.persona.particle.transformation
 import com.github.wangyung.persona.particle.MutableParticle
 
 /**
- * The transformation that updates the rotation by the rotational speed.
+ * The transformation that updates the rotation by the rotational speed. It only modifies the
+ * current particle state when everytime the [transform] is invoked.
+ * It would have different result even invokes [transform] multiple times with the same iteration.
  */
 class LinearRotationTransformation : ParticleTransformation {
     override fun transform(particle: MutableParticle, iteration: Long) {
---
+++ modules/persona/src/test/java/com/github/wangyung/persona/particle/transformation/LinearRotationTransformationTest.kt
@@ -0,0 +1,28 @@
+package com.github.wangyung.persona.particle.transformation
+
+import com.github.wangyung.persona.particle.Instinct
+import com.github.wangyung.persona.particle.MutableParticle
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class LinearRotationTransformationTest {
+
+    lateinit var mockParticle: MutableParticle
+    lateinit var linearRotationTransformation: LinearRotationTransformation
+
+    @Before
+    fun setUp() {
+        mockParticle = MutableParticle(id = 300, instinct = Instinct(rotationalSpeed = 10f))
+        linearRotationTransformation = LinearRotationTransformation()
+    }
+
+    @Test
+    fun `Update the rotation of particle correctly`() {
+        // when
+        linearRotationTransformation.transform(mockParticle, 1)
+
+        // then
+        assertEquals(10f, mockParticle.rotation)
+    }
+}
