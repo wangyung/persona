66125e138d29c4cbbc7457c1c34756bcee4b125d
Freddie Wang <wangyung@gmail.com>
2022-01-10 00:21:47 +0900

    Add new module for json serialization

---
+++ modules/persona-json-serialization-kotlinx/src/main/java/com/github/wangyung/persona/json/JsonLoader.kt
@@ -0,0 +1,9 @@
+package com.github.wangyung.persona.json
+
+import com.github.wangyung.persona.particle.generator.parameter.RandomizeParticleGeneratorParameters
+
+class JsonLoader {
+    fun load(): RandomizeParticleGeneratorParameters {
+        TODO("Not yet implemented")
+    }
+}
